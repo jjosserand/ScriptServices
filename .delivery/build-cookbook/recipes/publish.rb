c6f6329bd9bcba5483c71d7c8c76eb586212ec70
Andre Elizondo <aelizondo@chef.io>
2016-05-11 16:22:55 -0700

    code yey

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -5,7 +5,10 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 powershell_script "clean-workspace" do
-    command "& \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" /Clean"
+#    command "& \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" /Clean"
+    code <<-EOH
+        & "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe" /Clean
+    EOH
     cwd '..\..\..\src'
 end
 
@@ -15,7 +18,10 @@ execute "restore-nuget-packages" do
 end
 
 powershell_script "build-solution" do
-    command "& \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" ScriptServices.sln /Build debug /Out log.txt"
+#    command "& \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" ScriptServices.sln /Build debug /Out log.txt"
+    code <<-EOH
+        & "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe" ScriptServices.sln /Build debug /Out log.txt
+    EOH
     cwd '..\..\..\src'
 end
 

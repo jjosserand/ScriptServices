f7182be7776dd7cf5e69de7e5d35c58a3da2550f
Andre Elizondo <aelizondo@chef.io>
2016-05-11 15:56:37 -0700

    publish

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+remote_file "#{Chef::Config[:file_cache_path]}/nuget.exe" do
+    source 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
+end
--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -3,3 +3,18 @@
 # Recipe:: publish
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+powershell_script "clean-workspace" do
+    command "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe /Clean"
+    cwd '..\..\..\src'
+end
+
+execute "restore-nuget-packages" do
+    command "#{Chef::Config[:file_cache_path]}/nuget.exe restore"
+    cwd '..\..\..\src'
+end
+
+powershell_script "build-solution" do
+    command '& "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ScriptServices.sln /Build debug /Out log.txt'
+    cwd '..\..\..\src'
+end

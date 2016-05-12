c8050d5fba449faf559727576cdd7a6e686314d8
Andre Elizondo <aelizondo@chef.io>
2016-05-11 17:00:12 -0700

    almost ready

--- .delivery/build-cookbook/attributes/default.rb
+++
@@ -1,1 +0,0 @@
-override['delivery']['workspace_path'] = "C:/dot-net"
--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,17 +4,19 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+src_dir = File.expand_path('../../src')
+
 powershell_script "clean-workspace" do
 #    command "& \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" /Clean"
     code <<-EOH
-        & "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe" /Clean
+        & "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe" ScriptServices.sln /Clean
     EOH
-    cwd 'C:/dot-net/ScriptServices/src'
+    cwd src_dir
 end
 
 execute "restore-nuget-packages" do
     command "#{Chef::Config[:file_cache_path]}\\nuget.exe restore"
-    cwd 'C:/dot-net/ScriptServices/src'
+    cwd src_dir
 end
 
 powershell_script "build-solution" do
@@ -22,11 +24,5 @@ powershell_script "build-solution" do
     code <<-EOH
         & "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe" ScriptServices.sln /Build debug /Out log.txt
     EOH
-    cwd 'C:/dot-net/ScriptServices/src'
-end
-
-powershell_script "show-build-log" do
-    command "gc log.txt -raw"
-    cwd 'C:/dot-net/ScriptServices/src'
-    live_stream true
+    cwd src_dir
 end

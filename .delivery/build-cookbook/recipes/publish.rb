a59c39c0d08b3e58988c922779d1695607ca17d4
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-06-08 11:09:58 +0100

    Output is saved to a log file

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -13,7 +13,7 @@ end
 
 powershell_script "build-solution" do
     code <<-EOH
-        & "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe" ScriptServices.sln 
+        & "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe" ScriptServices.sln /t:go /fl /flp:logFile=ScriptServices.build.log
     EOH
     cwd src_dir
 end
